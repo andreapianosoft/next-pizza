33952eb9827cd1242c57c831a97bffbe1599fcb5
Andrea Piano <andrea.piano@softfobia.com>
2022-11-09 15:39:21 +0100

    Add new product

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import axios from 'axios'
 import Head from 'next/head'
 import Featured from '../components/Featured'
 import PizzaList from '../components/PizzaList'
 import styles from '../styles/Home.module.css'
+import Add from '../components/Add'
+import AddButton from '../components/AddButton'
 
 
-export default function Home({pizzaList}) {
-
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true);
 
   return (
     <div className={styles.container}>
@@ -16,17 +19,25 @@ export default function Home({pizzaList}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose} />}
       <PizzaList pizzaList={pizzaList} />
+      {!close && <Add setClose={setClose} />}
     </div>
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+  
+  if ( myCookie.token === process.env.TOKEN ) {
+    admin = true;
+  }
   const res = await axios.get("http://localhost:3000/api/products");
   return {
     props: {
       pizzaList: res.data,
-
+      admin
     }
   }
 }
